f3bee2915841b92fee326ccbb9333152af301496
Alejandro de la Rubia <alejandrodelarubiam@gmail.com>
2023-03-22 15:39:32 +0100

    app.py after linting

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
+import logging
 from flask import Flask
 from flask import json
-import logging
-
+from flask.logging import create_logger
 app = Flask(__name__)
+LOG = create_logger(app)
 
 @app.route('/status')
 def healthcheck():
     response = app.response_class(
-            response=json.dumps({"result":"OK - healthy"}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
     )
 
-    app.logger.info('Status request successfull')
+    LOG.info('Status request successfull')
     return response
 
 @app.route('/metrics')
 def metrics():
     response = app.response_class(
-            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
-            status=200,
-            mimetype='application/json'
+        response=json.dumps({"status":"success", "code":0, "data":{"UserCount":140, "UserCountActive":23}}),
+        status=200,
+        mimetype='application/json'
     )
 
-    app.logger.info('Metrics request successfull')
+    LOG.info('Metrics request successfull')
     return response
 
 @app.route("/")
 def hello():
-    app.logger.info('Main request successfull')
+    LOG.info('Main request successfull')
 
     return "Hello World!"
 
 if __name__ == "__main__":
     ## stream logs to a file
     logging.basicConfig(filename='app.log',level=logging.DEBUG)
-    
     app.run(host='0.0.0.0', port=80)
