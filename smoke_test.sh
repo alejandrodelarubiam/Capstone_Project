d32a0552516f0290ab8f774da6eaabc77696519e
Alejandro de la Rubia <alejandrodelarubiam@gmail.com>
2023-03-28 21:07:03 +0200

    Tryinggg

--- smoke_test.sh
+++ smoke_test.sh
@@ -1,6 +1,7 @@
-CIRCLE_WORKFLOW_ID=4771e45
+CIRCLE_WORKFLOW_ID=d73f1ee
 aws eks --region us-east-1 update-kubeconfig --name CapstoneADLRM-Cluster-${CIRCLE_WORKFLOW_ID}
 URL=$(kubectl get svc capstoneadlrm-microservice-${CIRCLE_WORKFLOW_ID} -o wide | awk '{print $4}' | tail -n 1)
+echo $URL
 curl -s ${URL}:8000         
 if curl -s ${URL}:8000 | grep "Hello World!"
 then
